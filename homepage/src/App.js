6b3c2b437cf87926db7dd7fdb720e72b599ed998
Matthew Brandon Thompson <matthew.brandon.thompson@gmail.com>
2020-03-03 14:32:00 -0700

    Working Through 140A

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,27 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComp from './components/MyComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am in control of this application and my name is Matthew Brandon</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "TBD"
+    };
+  }
+  onPushMe = () => {
+    console.log("You pushed me");
+    this.counter++;
+    this.setState({
+      myState: "now:" + this.counter
+    });
+  }
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>I am in control of this application and my name is Matthew Brandon {this.counter} {this.state.myState}</h1>
+          <button onClick={this.onPushMe}>
+              Push Me
+            </button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        {/* <MyComp/> */}
+      </div>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>I am in control of this application and my name is Matthew Brandon {counter} {state.myState} </h1>
+//         <button onClick={onPushMe}>
+//             Push Me
+//           </button>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
